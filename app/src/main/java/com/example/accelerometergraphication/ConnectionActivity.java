df9e44f95dd91149099e59169fd7d06355887a7e
Johan Garrido <johangaromen@gmail.com>
2021-05-09 14:20:26 -0500

    -v0.0.2 Conection activity now receives intent device and displays device name.

--- app/src/main/java/com/example/accelerometergraphication/ConnectionActivity.java
+++ app/src/main/java/com/example/accelerometergraphication/ConnectionActivity.java
@@ -2,13 +2,25 @@ package com.example.accelerometergraphication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ConnectionActivity extends AppCompatActivity {
+    TextView deviceNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+        final Intent intent = getIntent();
+        final BluetoothDevice device = intent.getParcelableExtra("Example item");
+
+        deviceNameTextView = findViewById(R.id.device_name);
+
+        deviceNameTextView.setText("Device Name: " + device.getName());
+
     }
 }
